4f6bfaa41180cb68a7362ed2194f5377a94ba302
josimar <jram@gft.com>
2025-02-19 12:18:27 -0300

    aula 22 completa

--- 00-baseHtmlJs/src/script.js
+++ 00-baseHtmlJs/src/script.js
@@ -1,10 +1,21 @@
-document.addEventListener('hello', function(data){
-  console.log('hello has called and send ', data.detail)
-})
-
-function callCustomMethod(){
-  let event = new CustomEvent('hello', {
-    detail:{name:'nikhil'}
-  })
-  document.dispatchEvent(event)
+//Problema que Arrow Function resolve
+let obj = {
+  name: 'nikhil',
+  getName:function(){
+    console.log(this.name)
+  }
 }
+obj.getName()
+//o problema começa quando a função se torna um aninhamento
+let obj2 = {
+  name1: 'nikhil2',
+  getName:function(){
+    console.log(this.name1) //nikhil2
+    const fullName = () =>{
+      console.log(this.name1) //nikhil2
+      console.log('my full name is ' + this.name1 + ' jkarek') //my full name is nikhil2 jkarek
+    }
+    fullName()
+  }
+}
+obj2.getName()
