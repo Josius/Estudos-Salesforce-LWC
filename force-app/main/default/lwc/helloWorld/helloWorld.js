8526bf8cc63248ac72945817ddc6c1cc5a5bede7
josimar <jram@gft.com>
2025-04-11 09:49:06 -0300

    aula 34 completa

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+	fullName = "Zero to Hero";
+	users = ["a", "b", "c"];
+	obj = {
+		name: "Jorge",
+		age: "25"
+	}
+}
