1792655f2ec8bebcef2c80ad0b125d2f245553f2
josimar <jram@gft.com>
2025-04-14 11:22:38 -0300

    aula 36 completa

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,14 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
 	fullName = "Zero to Hero";
-	users = ["a", "b", "c"];
-	obj = {
-		name: "Jorge",
-		age: "25"
-	}
 	title = "Aura";
 	changeHandler(event){
 		this.title = event.target.value;
+	}
+
+	address={
+		city: "Melbourne",
+		country: 'Australia',
+		postalCode: 3008
+	}
+	
+	trackHandler(event){
+		this.address = {...this. address, "city": event.target.value};
 	}
 }
