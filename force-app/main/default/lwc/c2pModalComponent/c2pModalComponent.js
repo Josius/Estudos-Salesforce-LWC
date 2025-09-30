7a39ee30a13b071014b345e80d9d1185cff89efa
Josius <josius.amaro@gmail.com>
2025-09-30 11:10:08 -0300

    aula 65 completa

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,7 +3,11 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement {
 
     closeHandler(){ 
-        const myEvent = new CustomEvent('close')
+        const myEvent = new CustomEvent('close', { 
+            detail: { 
+                msg:"Modal Closed Successfully!!"
+            }
+        })
         this.dispatchEvent(myEvent)
     }
 }
